9353194b81d8af30af6948c2b6279177314ca4c6
Peter Steinberg <bklynpeter@gmail.com>
2025-03-15 11:22:06 -0400

    add github link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Header = ({ className }: { className: string }) => {
   return (
@@ -12,10 +13,27 @@ const Header = ({ className }: { className: string }) => {
       }}
       className={className}
     >
-      <Container maxWidth='lg' sx={{ padding: 1 }}>
+      <Container
+        maxWidth='lg'
+        sx={{
+          padding: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
         <Typography variant='h4' color='white' fontWeight='bold'>
           GRAPHCOIN
         </Typography>
+        <a
+          href='https://github.com/steinbergpeter/graphcoin'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GitHubIcon
+            sx={{ color: 'white', fontSize: 30, cursor: 'pointer' }}
+          />
+        </a>
       </Container>
     </Box>
   );
